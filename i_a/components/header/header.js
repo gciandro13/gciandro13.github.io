bc6f85aa5564d892a689c52964786c2168ccfc21
Giovanni Ciandrini <g.ciandrini@websolute.it>
2021-01-29 19:13:44 +0100

    Page-Header Template html/css

--- i_a/components/header/header.js
+++ i_a/components/header/header.js
@@ -3,37 +3,90 @@ class PageHeader extends HTMLElement {
         super();
 
         const template = `
-           <head>
-           <link rel="stylesheet" href="./components/header/header.css"/>
-           </head>
-           <body>
-            <a href="/I_A">
-                <img class="logo-img" alt="logo" />
-            </a>
-<div id='cssmenu'>
-<ul>
-   <li><a href='#'><span>Home</span></a></li>
-   <li class='active has-sub'><a href='#'><span>Products</span></a>
-      <ul>
-         <li class='has-sub'><a href='#'><span>Product 1</span></a>
-            <ul>
-               <li><a href='#'><span>Sub Product</span></a></li>
-               <li class='last'><a href='#'><span>Sub Product</span></a></li>
-            </ul>
-         </li>
-         <li class='has-sub'><a href='#'><span>Product 2</span></a>
-            <ul>
-               <li><a href='#'><span>Sub Product</span></a></li>
-               <li class='last'><a href='#'><span>Sub Product</span></a></li>
-            </ul>
-         </li>
-      </ul>
-   </li>
-   <li><a href='#'><span>About</span></a></li>
-   <li class='last'><a href='#'><span>Contact</span></a></li>
-</ul>
-</div>
-</body>
+        <link href="styles/bootstrap.min.css" rel="stylesheet" />
+        <link href="styles/style.css" rel="stylesheet" />
+        <link href="styles/font-awesome.min.css" rel="stylesheet">
+        <link href="styles/skin/green.css" rel="stylesheet">
+        <link href="styles/skin/green.css" rel="stylesheet">
+        <script src="assets/js/main_script.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <div class="top-bar">
+            <div class="container">
+                <div class="tsocial">
+                    <a href=""><i class="fa fa-facebook"></i></a>
+                    <a href=""><i class="fa fa-twitter"></i></a>
+                    <a href=""><i class="fa fa-linkedin"></i></a>
+                    <a href=""><i class="fa fa-youtube"></i></a>
+                    <a href=""><i class="fa fa-skype"></i></a>
+                </div>
+            </div>
+        </div>
+
+    <div class="menu-holder clearfix">
+        <div class="menu-area">
+            <div class="container">
+                <div class="row">
+
+                    <!-- Navigation starts -->
+                    <div class="col-md-12">
+                        <div class="mainmenu">
+                            <div class="navbar navbar-nobg">
+                            
+                                <div class="navbar-header">
+                                    <a class="navbar-brand" href="index.html"><img src="assets/img/logo.png" alt=""></a>
+                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                </div>
+
+                                <div class="navbar-collapse collapse">
+                                    <nav>
+                                        <ul class="nav navbar-nav navbar-right">
+                                        
+                                        <li class="active"><a class="smooth_scroll" href="#slider">Home</a></li>
+                                        <li class="dropdown">
+                                          <a href="#" class="smooth_scroll dropdown-toggle" data-toggle="dropdown">Chi siamo<b class="caret"></b></a>
+                                          <ul class="dropdown-menu">
+                                             <li><a href="#">Le nostre persone</a></li>
+                                             <li><a href="#">La nostra azienda</a></li>
+                                          </ul>
+                                        </li>
+                                        <li><a class="smooth_scroll" href="#">Mercati</a></li>
+                                        <li><a class="smooth_scroll" href="#">Referenze</a></li>
+                                        <li class="dropdown">
+                                          <a href="#" class="smooth_scroll dropdown-toggle" data-toggle="dropdown">Progetti<b class="caret"></b></a>
+                                          <ul class="dropdown-menu">
+                                             <li><a href="#">Opere di urbanizzazione</a></li>
+                                             <li><a href="#">Opere fluviali</a></li>
+                                             <li><a href="#">Opere fognarie</a></li>
+                                             <li><a href="#">Opere idriche</a></li>
+                                             <li><a href="#">Opere impiantistiche</a></li>
+                                             <li><a href="#">Studi idraulici</a></li>
+                                          </ul>
+                                        </li>
+                                        <li class="dropdown">
+                                          <a href="#" class="smooth_scroll dropdown-toggle" data-toggle="dropdown">Media<b class="caret"></b></a>
+                                          <ul class="dropdown-menu">
+                                             <li><a href="#">News</a></li>
+                                             <li><a href="#">Downloads</a></li>
+                                          </ul>
+                                        </li>
+                                        <li><a class="smooth_scroll" href="#work">Contatti/Unisciti a noi </a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
         `;
 
         let shadow = this.attachShadow({
